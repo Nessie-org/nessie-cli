e83ebe9a0e129cd4a759a9b5f6a1bdc804ca0346
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-03-12 03:54:15 +0100

    Ref: move show_exp to Interpreter

--- nessie_cli/interpreter.py
+++ nessie_cli/interpreter.py
@@ -16,21 +16,23 @@ class Interpreter:
                 print(f" - Argument: {arg}")
                 print(f"   - Type: {type(arg).__name__}")
                 if arg.__class__.__name__.endswith("Expression"):
-                    show_exp(arg)
+                    self._show_exp(arg)
             for kwarg in command.kwargs:
                 print(f" - Keyword Argument: {kwarg.key} = {kwarg.value}")
 
-
-def show_exp(exp, indent=0):
-    print("  " * indent + f"Expression: {type(exp).__name__}")
-    print("  " * indent + f"Operator: {getattr(exp, 'op', None)}")
-    if hasattr(exp, "left"):
-        print("  " * (indent + 1) + "Left:")
-        show_exp(exp.left, indent + 2)
-    if hasattr(exp, "right"):
-        print("  " * (indent + 1) + "Right:")
-        if isinstance(exp.right, list):
-            for e in exp.right:
-                show_exp(e, indent + 2)
-        else:
-            show_exp(exp.right, indent + 2)
+    def _show_exp(self, exp, indent=0):
+        print("  " * indent + f"Expression: {type(exp).__name__}")
+        if isinstance(exp, (int, float, str)):
+            print("  " * indent + f"Value: {exp}")
+            return
+        print("  " * indent + f"Operator: {getattr(exp, 'op', None)}")
+        if hasattr(exp, "left"):
+            print("  " * (indent + 1) + "Left:")
+            self._show_exp(exp.left, indent + 2)
+        if hasattr(exp, "right"):
+            print("  " * (indent + 1) + "Right:")
+            if isinstance(exp.right, list):
+                for e in exp.right:
+                    self._show_exp(e, indent + 2)
+            else:
+                self._show_exp(exp.right, indent + 2)
